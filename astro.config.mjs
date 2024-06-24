742a40d4736942437da991f393a14271ec5a1f39
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-06-23 20:50:15 -0700

    First approach of para-no-programadores

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,28 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Programación desde 0',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/jesusdmedinac',
 			},
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+        {
+          label: 'Para no programadores',
+          items: [
+            { label: 'Antes de empezar', link: '/roadmap/para-no-programadores/1' },
+            { label: 'Conceptos básicos', link: '/roadmap/para-no-programadores/2'},
+            { label: 'Language Tour', link: '/roadmap/para-no-programadores/3'},
+            { label: 'Paradigmas', link: '/roadmap/para-no-programadores/4'},
+            { label: 'Stacks', link: '/roadmap/para-no-programadores/5'},
+            { label: 'Frontend', link: '/roadmap/para-no-programadores/6'},
+            { label: 'Backend', link: '/roadmap/para-no-programadores/7'},
+            { label: 'Base de datos', link: '/roadmap/para-no-programadores/8'},
+            { label: 'Uso de terminal', link: '/roadmap/para-no-programadores/9'},
+            { label: 'Controles de versión', link: '/roadmap/para-no-programadores/10'},
+            { label: 'Sistemas operativos', link: '/roadmap/para-no-programadores/11'},
+            { label: 'DevOps', link: '/roadmap/para-no-programadores/12'},
+          ]
+        },
 			],
 		}),
 	],
---
+++ src/content/docs/roadmap/para-no-programadores.md
@@ -0,0 +1,17 @@
+---
+title: Programación desde 0 - Para no programadores
+description: Ruta de aprendizaje de programación desde 0
+---
+
+1. Antes de empezar
+2. Conceptos básicos
+3. Language Tour
+4. Paradigmas
+5. Stacks
+6. Frontend
+7. Backend
+8. Base de datos
+9. Uso de la Terminal
+10. Controles de versión
+11. Sistemas operativos
+12. DevOps
