59996609c4c55ceae71d23c610da50deef08ad22
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-08-15 13:12:00 -0700

    Platica con Maxister Button

--- src/maxister/chat.button.jsx
+++ src/maxister/chat.button.jsx
@@ -3,7 +3,7 @@ import { LuSparkles } from "react-icons/lu";
 export function ChatButton() {
   return (
     <div className="chat-button-container">
-      <label className="chat-label bg-white shadow-lg rounded-full px-2 py-1">Platica con Maxister</label>
+      <label className="chat-label bg-white shadow-lg rounded-full px-2 py-1">{process.env.GEMINI_API_KEY}</label>
       <button className="chat-button rounded-full bg-paradiso hover:bg-fountain-blue text-white size-16 shadow-lg">
         <LuSparkles className="chat-icon size-8" />
       </button>
