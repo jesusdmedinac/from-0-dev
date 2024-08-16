504455fb6ef56644d74b3348375a4dec3c679230
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-08-15 22:50:02 -0700

    Scroll to bottom on new message

--- src/maxister/chat/components/ChatMessages.jsx
+++ src/maxister/chat/components/ChatMessages.jsx
@@ -1,26 +1,31 @@
+import { useEffect, useRef } from 'react';
 import Markdown from 'react-markdown'
 
 export default function ChatMessages({ messages }) {
+  let lastMessageRef = useRef(null);
+  useEffect(() => {
+    const {current} = lastMessageRef
+    if (current !== null){
+      current.scrollIntoView({behavior: "smooth", block: "end"})
+    }
+    return () => {
+      lastMessageRef = null;
+    }
+  }, [messages]);
   return (
     <>
       {/* Área de Chat */}
       <main className="flex-grow overflow-y-auto p-4 bg-botticelli">
         {messages.map((message, index) => {
-          let author = "";
-          if (message.role == "user") {
-            author = "Alumno"
-          } else if (message.role == "model") {
-            author = "Maxister"
-          }
           const markdown = message.content;
           return (
             message.role == "user" 
-            ? <div key={index} className="mb-4 text-right">
+            ? <div key={index} className="mb-4 text-right" ref={lastMessageRef}>
               <div className="bg-paradiso text-white p-3 rounded-lg shadow-md max-w-md ml-auto">
                 <Markdown>{markdown}</Markdown>
               </div>
             </div>
-            : <div key={index} className="mb-4">
+            : <div key={index} className="mb-4" ref={lastMessageRef}>
               <div className="bg-viking text-white p-3 rounded-lg shadow-md max-w-md">
                 <Markdown>{markdown}</Markdown>
               </div>
